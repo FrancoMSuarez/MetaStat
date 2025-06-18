bea69cd5b59a58b8c848bd59c63aedc7d9f79594
FrancoMSuarez <franmarce.3@gmail.com>
2025-06-18 16:36:55 -0300

    Update

--- R/app_ui.R
+++ R/app_ui.R
@@ -12,7 +12,7 @@ app_ui <- function(request) {
 
     # Your application UI logic
     bslib::page_navbar(
-      title = "MetaStats",
+      title = "MetaStat",
       id = "navbar",
       theme = bslib::bs_theme(bootswatch = "minty"),
 
@@ -176,6 +176,7 @@ golem_add_external_resources <- function() {
 
   tags$head(
     tags$link(rel = "shortcut icon", href = "www/favicon.png"),
+    tags$link(rel = "stylesheet", type = "text/css", href = "www/style.css"),
     bundle_resources(
       path = app_sys('app/www'),
       app_title = 'MetaStat'
--- R/run_app.R
+++ R/run_app.R
@@ -14,6 +14,7 @@ run_app <- function(
   uiPattern = "/",
   ...
 ) {
+  #shiny::addResourcePath("www", system.file("app/www", package = "MetaStat"))
   golem::with_golem_options(
     app = shinyApp(
       ui = app_ui,
